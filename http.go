cb415b875b7371b52d768d539af875a68b385257
gypsydiver <fermendozarcs@gmail.com>
2019-05-22 16:55:56 +0200

    add instrumentation layer

--- http.go
+++ http.go
@@ -1,26 +1,37 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "net/http"
-  "log"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "8080"
-    }
+	requestsProcessed := promauto.NewCounter(prometheus.CounterOpts{
+		Name: "requests_processed_total",
+		Help: "The total number of processed requests",
+	})
 
-    fmt.Fprintf(os.Stdout, "Listening on :%s\n", port)
-    hostname, _ := os.Hostname()
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(os.Stdout, "I'm %s\n", hostname)
- 	fmt.Fprintf(w, "I'm %s\n", hostname)
-    })
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
+	fmt.Fprintf(os.Stdout, "Listening on :%s\n", port)
 
-    log.Fatal(http.ListenAndServe(":" + port, nil))
+	hostname, _ := os.Hostname()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		defer requestsProcessed.Inc()
+		fmt.Fprintf(os.Stdout, "I'm %s\n", hostname)
+		fmt.Fprintf(w, "I'm %s\n", hostname)
+	})
+
+	http.Handle("/metrics", promhttp.Handler())
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
-
